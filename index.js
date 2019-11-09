30bdb4e1a06ce261a074e8f835475cd18832e478
Jean Tubay <jean.tubay.92@gmail.com>
2019-11-09 16:06:40 -0500

    adding api and npm needed

--- index.js
+++ index.js
@@ -2,15 +2,31 @@
 
 const axios = require("axios");
 const inquirer = require("inquirer");
+const fs = require('fs')
 inquirer.prompt([
   {
     type: "input",
     message: "Please enter your username:",
     name: "username"
+  },
+  {
+      type: 'list',
+      name: 'color',
+      message: 'what is your favorite color?',
+      choices: ['red', 'green', 'blue']
   }
 ]).then( answers => {
   axios.get(`https://api.github.com/users/${answers.username}`)
     .then( response => {
       console.log(response);
+      console.log(answers.color)
+    
+      fs.writeFile(response.name + ".json", JSON.stringify(answers, null, 2), err => {
+          if(err) {
+              return console.log("err")
+          }
+          console.log("done")
+      })
+
     });
 });
