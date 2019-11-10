124b49e69be8fd3e17e0d258afce092f267e7f64
Jean Tubay <jean.tubay.92@gmail.com>
2019-11-10 15:42:12 -0500

    adding format for creating md file

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 
-const MarkdownIt = require('markdown-it')
-const md = new MarkdownIt()
 
 const fs = require('fs')
 inquirer.prompt([
@@ -18,7 +16,7 @@ inquirer.prompt([
       type: 'list',
       name: 'color',
       message: 'what is your favorite color?',
-      choices: ['red', 'green', 'blue']
+      choices: ['red', 'green', 'blue', 'purple', 'pink', 'orange']
   }
 ]).then( answers => {
   axios.get(`https://api.github.com/users/${answers.username}`)
@@ -32,31 +30,16 @@ inquirer.prompt([
       console.log(response.data.following)
       console.log(response.data.public_repos)
       console.log(response.data.avatar_url)
+      const imageUrl = response.data.avatar_url
       
 
       
 
-      //const test = `#${response.data.bio}___${response.data.name}___${response.data.company}`
-      
-     //const test = md.render(`# ${response.data.name} ${response.data.bio}${response.data.following}`)
-
-     const test = md.render(`
-
----
-
-# <span style="color:${answers.color}"${response.data.name}</span>
-## ${response.data.bio}
-### !['person'](${response.data.avatar_url})
-#### h4 Heading
-##### h5 Heading
-###### h6 Heading
-
-
-## Horizontal Rules
-     `)
+  
+     const test = `<div style=background-color:grey;><h1 style=color:${answers.color}>${response.data.name}</h1>![avatar](${imageUrl})<h3>Bio: ${response.data.bio}</h3><h4>Repo URL: ${response.data.repos_url}</h4><h4>Public Repos: ${response.data.public_repos}</h4><h4>Follower: ${response.data.followers}</h4><h4>Following: ${response.data.following}</h4><h4>Location: ${response.data.location}</h4></div>`
 
     
-      fs.writeFile("me.md", JSON.stringify(test), err => {
+      fs.writeFile("output.md", JSON.stringify(test), err => {
           if(err) {
               return console.log("err")
           }
